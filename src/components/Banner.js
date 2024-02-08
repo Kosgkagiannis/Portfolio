be37c1991526be4f46578a4fc45dcec0cc5237c9
Kosgkagiannis <kosgkagiannis@gmail.com>
2024-02-08 18:40:11 +0100

    Check banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,72 +18,70 @@ export const Banner = () => {
       >
         <SpaceScene />
       </Canvas>
-      <section className="banner" id="home">
-        <Container>
-          <Row>
-            <Col xs={12} md={6} xl={7}>
-              <TrackVisibility once>
-                {({ isVisible }) => (
-                  <>
-                    <div
-                      className={
-                        isVisible
-                          ? "animate__animated animate__zoomIn"
-                          : "animate__animated animate__zoomOut"
-                      }
-                    >
-                      <span className="tagline">Welcome to my Portfolio</span>
-                    </div>
-                    <div
-                      className={
-                        isVisible
-                          ? "animate__animated animate__zoomInLeft"
-                          : "animate__animated animate__zoomOutRight"
-                      }
-                    >
-                      <h1>{`Konstantinos Gkagiannis`}</h1>
-                    </div>
-                    <div
-                      className={
-                        isVisible
-                          ? "animate__animated animate__zoomInRight"
-                          : "animate__animated animate__zoomOutLeft"
-                      }
-                    >
-                      <p>
-                        I hold a Master's degree in Computer Engineering and
-                        Informatics, specializing in various programming
-                        languages. My experience as a Front End Developer has
-                        provided me with a solid foundation, and hands-on work
-                        has enhanced my ability to adapt and innovate. I am
-                        enthusiastic about using technology to tackle real-world
-                        challenges.
-                      </p>
-                    </div>
-                  </>
-                )}
-              </TrackVisibility>
-            </Col>
-            <Col xs={12} md={6} xl={5}>
-              <TrackVisibility once>
-                {({ isVisible }) => (
+      <Container>
+        <Row>
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility once>
+              {({ isVisible }) => (
+                <>
                   <div
                     className={
                       isVisible
-                        ? "animate__animated animate__fadeIn"
-                        : "animate__animated animate__fadeOut"
+                        ? "animate__animated animate__zoomIn"
+                        : "animate__animated animate__zoomOut"
                     }
                   >
-                    <div className="planet-container">
-                      <RotatingPlanetCanvas />
-                    </div>
+                    <span className="tagline">Welcome to my Portfolio</span>
                   </div>
-                )}
-              </TrackVisibility>
-            </Col>
-          </Row>
-        </Container>
-      </section>
+                  <div
+                    className={
+                      isVisible
+                        ? "animate__animated animate__zoomInLeft"
+                        : "animate__animated animate__zoomOutRight"
+                    }
+                  >
+                    <h1>{`Konstantinos Gkagiannis`}</h1>
+                  </div>
+                  <div
+                    className={
+                      isVisible
+                        ? "animate__animated animate__zoomInRight"
+                        : "animate__animated animate__zoomOutLeft"
+                    }
+                  >
+                    <p>
+                      I hold a Master's degree in Computer Engineering and
+                      Informatics, specializing in various programming
+                      languages. My experience as a Front End Developer has
+                      provided me with a solid foundation, and hands-on work has
+                      enhanced my ability to adapt and innovate. I am
+                      enthusiastic about using technology to tackle real-world
+                      challenges.
+                    </p>
+                  </div>
+                </>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility once>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible
+                      ? "animate__animated animate__fadeIn"
+                      : "animate__animated animate__fadeOut"
+                  }
+                >
+                  <div className="planet-container">
+                    <RotatingPlanetCanvas />
+                  </div>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
